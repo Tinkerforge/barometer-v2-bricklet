e013aaf14c37cccd310a85169ddc6e4473131f0b
Erik Fleckstein <erik@tinkerforge.com>
2018-12-18 15:15:30 +0100

    go: Change import path again.

--- software/examples/go/example_callback.go
+++ software/examples/go/example_callback.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/tinkerforge/go-api-bindings/barometer_v2_bricklet"
-	"github.com/tinkerforge/go-api-bindings/ipconnection"
+	"github.com/Tinkerforge/go-api-bindings/barometer_v2_bricklet"
+	"github.com/Tinkerforge/go-api-bindings/ipconnection"
 )
 
 const ADDR string = "localhost:4223"
